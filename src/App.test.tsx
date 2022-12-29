a4bf022e0f90cf18b4b370711b496e23df6ca1ec
Gennadiy Magomev <g.a.magomaev@gmail.com>
2022-12-29 11:19:53 +0300

    feat(JS-3): added deploy workflow called main-workflow.yml

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -13,7 +13,7 @@ describe("Application testing", () => {
 
       root.render(<App />);
 
-      const app = screen.getByText(/App deployed/i);
+      const app = screen.getByText(/App deployed test/i);
 
       // eslint-disable-next-line jest/no-conditional-expect
       expect(app).toBeInTheDocument();
